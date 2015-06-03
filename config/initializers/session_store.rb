138f4169fbb3fe21a87f8c808e2e846adb8c476c
Hosam Aly <hosamaly6@gmail.com>
2015-06-03 16:52:07 +0200

    Rename the app to 'sample_ror-csv-haml'

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_csv_test_session'
+Rails.application.config.session_store :cookie_store, key: '_sample_ror_csv_haml_session'
